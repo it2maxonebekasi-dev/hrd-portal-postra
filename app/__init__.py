ea1336c519717e1e2c0880cba193b4272aa3642e
it2maxonebekasi-dev <it.2.maxonebekasi@gmail.com>
2026-06-26 11:43:50 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,10 @@ def create_app():
     # -------------------- Konfigurasi dasar --------------------
     # Mengambil SECRET_KEY dari environment Render jika ada, fallback ke string biasa
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "kuncirahasia_superaman")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hrd_portal.db"
+    database_url = os.environ.get("DATABASE_URL", "sqlite:///hrd_portal.db")
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # ==========================================================
